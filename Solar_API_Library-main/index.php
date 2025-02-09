0f7757cd13c7c8a328dc8c57d8dfc68d483eb4f1
Gaya <gayahockmess@gmail.com>
2025-02-09 04:20:06 +0000

    commit all changes

--- Solar_API_Library-main/index.php
+++ Solar_API_Library-main/index.php
@@ -1,10 +1,3 @@
-<?php
-require 'vendor/autoload.php';
-Dotenv\Dotenv::createImmutable(__DIR__)->load();
-
-$solarApiKey = getenv('SOLAR_API_KEY');
-$mapsApiKey = getenv('MAPS_API_KEY');
-?>
 <!DOCTYPE html>
 <html lang="en">
 
